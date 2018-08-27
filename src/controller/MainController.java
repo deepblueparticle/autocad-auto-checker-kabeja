a44e6fac71f239c46a2e220c010edc1703ad9854
armelcaleja <armel.d.caleja@gmail.com>
2018-08-28 00:17:07 +0800

    get cirlce data

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -15,6 +15,7 @@ import java.io.FileInputStream;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+import org.kabeja.dxf.DXFCircle;
 import org.kabeja.dxf.DXFConstants;
 import org.kabeja.dxf.DXFDocument;
 import org.kabeja.dxf.DXFLayer;
@@ -32,7 +33,7 @@ public class MainController{
 	
 	public static void main(String[] args) throws FileNotFoundException {
 
-		String sourcepath ="C:\\Users\\caleja\\Desktop\\testdxf\\rectangle-exploded.dxf";
+		String sourcepath ="testdxf\\circle.dxf";
 		InputStream source = new FileInputStream(sourcepath);
 		
 		ArrayList<Vector3D> points = new ArrayList<Vector3D>();
@@ -49,7 +50,7 @@ public class MainController{
 			while (layerIterable.hasNext()) {
 				DXFLayer layer = (DXFLayer) layerIterable.next();
 				
-				System.out.println(layer.getLineType());
+				System.out.println("LAYER: " + layer.getName());
 				Iterator entityIterator = layer.getDXFEntityTypeIterator();
 				
 				if (entityIterator != null) {
@@ -87,7 +88,7 @@ public class MainController{
 									point = pline.getEndPoint();
 									Vector2D vector=new Vector2D(point.getX(), point.getY());
 //									points.add(vector);
-									System.out.println("vector point:" + vector);
+//									System.out.println("vector point:" + vector);
 									//BowlerStudioController.addCsg(new Cube(1,1,1).setCenter(center).toCSG());
 									System.out.print("\n");
 								}
@@ -122,57 +123,33 @@ public class MainController{
 							List plines = layer.getDXFEntities(entityType);
 							if (plines != null) {
 								for (Object p : plines) {
-									System.out.println("CIRCLE");
-//									System.out.println("X = " + point.getX() + ", Y = " + point.getY());
-//									System.out.println("length: " + pline.getLength());
+									System.out.println("ENTITY: " + entityType);
+									DXFCircle pline = (DXFCircle) p;
+									
+									Point centerpoint = pline.getCenterPoint();
+									double radius = pline.getRadius();
+									double topY = centerpoint.getY() - radius;
+									double bottomY = centerpoint.getY() + radius;
+									double leftX = centerpoint.getX() - radius;
+									double rightX = centerpoint.getX() + radius;
+									
+									System.out.println("center point: X,Y(" + centerpoint.getX() + ", " + centerpoint.getY() +")");
+									System.out.println("radius: " + radius);
+									System.out.println("topY: " + topY);
+									System.out.println("bottomY: " + bottomY);
+									System.out.println("leftX: " + leftX);
+									System.out.println("rightX: " + rightX);
+											
 								}
 							}
 						}
 					}
 				}
 			}
-			
-			
-//			System.out.println(doc.getDXFLayer(""));
-//			System.out.println(doc.getDXFLayer(""));
-//			for(int i = 0; i < doc.getDXFLayerIterator(); i++) {
-//				
-//			}
-//			DXFLayer layer = doc.getDXFLayer("");
-
-			//get all polylines from the layer
-//			List plines = layer.getDXFEntities(DXFConstants.ENTITY_TYPE_POLYLINE);
-//			System.out.println(plines);
-			//work with the first polyline
-//			DXFPolyline pline = plines;
-//			for (int i = 0; i < pline.getVertexCount(); i++) {
-//				    
-//			     DXFVertex vertex = pline.getVertex(i);
-//			                    
-//			     //do something like collect the data and
-//			     //build a mesh for a FEM system
-//		    }
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}	
 
-	}
-	
-	public void  read(InputStream in, String layerid) {
-
-   
-	}
-
-	public void doSomething(DXFPolyline pline) {
-
-	    //iterate over all vertex of the polyline
-	    for (int i = 0; i < pline.getVertexCount(); i++) {
-			    
-	     DXFVertex vertex = pline.getVertex(i);
-	                    
-	     //do something like collect the data and
-	     //build a mesh for a FEM system
-	    }
 	}
 }
