c722ca1d2b5ed61713d37549d0cc70a1518ffd8d
armelcaleja <armel.d.caleja@gmail.com>
2018-08-28 19:47:39 +0800

    Removed Vector3D implementation

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -7,14 +7,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.io.FileInputStream;
-
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
-import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 import org.kabeja.dxf.DXFCircle;
 import org.kabeja.dxf.DXFConstants;
 import org.kabeja.dxf.DXFDocument;
@@ -38,7 +31,6 @@ public class MainController{
 		String sourcepath ="testdxf\\circle.dxf";
 		InputStream source = new FileInputStream(sourcepath);
 
-		ArrayList<Vector3D> points = new ArrayList<Vector3D>();
 		Parser parser = ParserBuilder.createDefaultParser();
 		try {
 
@@ -68,9 +60,6 @@ public class MainController{
 										DXFVertex vertex = pline.getVertex(i);
 										Point point = vertex.getPoint();
 										System.out.println("X = " + point.getX() + ", Y = " + point.getY() + ",Z = " +point.getZ());
-//										Vector3D center=new Vector3D(point.getX(), point.getY(), point.getZ());
-//										points.add(center);
-//										BowlerStudioController.addCsg(new Cube(1,1,1).setCenter(center).toCSG());
 									}
 								}
 							}
@@ -86,18 +75,10 @@ public class MainController{
 									System.out.println("X = " + point.getX() + ", Y = " + point.getY());
 									System.out.println("length: " + pline.getLength());
 
-									points.add(new Vector3D(point.getX(), point.getY(), point.getZ()));
 									point = pline.getEndPoint();
-									Vector2D vector=new Vector2D(point.getX(), point.getY());
-//									points.add(vector);
-//									System.out.println("vector point:" + vector);
-									//BowlerStudioController.addCsg(new Cube(1,1,1).setCenter(center).toCSG());
 									System.out.print("\n");
 								}
 							}
-//							if(points.size()>0)
-//								parts.add(Extrude.points(new Vector3D(0, 0, extrudeDistance), points));
-							points.clear();
 						}
 						else if (entityType.contentEquals(DXFConstants.ENTITY_TYPE_SPLINE)) {
 							// get all polylines from the layer
@@ -109,16 +90,9 @@ public class MainController{
 									if(splinePointIterator!=null)
 										for (;splinePointIterator.hasNext();) {
 											SplinePoint point =(SplinePoint) splinePointIterator.next();
-											Vector3D vector = new Vector3D(point.getX(), point.getY(), point.getZ());
-											points.add(vector);
-											System.out.println("ENTITY_TYPE_SPLINE Point at " + vector);
-											//BowlerStudioController.addCsg(new Cube(1,1,1).setCenter(center).toCSG());
 										}
 								}
 							}
-//							if(points.size()>0)
-//								parts.add(extrude(new Vector3D(0, 0, extrudeDistance), points));
-							points.clear();
 						}
 						else if (entityType.contentEquals(DXFConstants.ENTITY_TYPE_CIRCLE)) {
 							List plines = layer.getDXFEntities(entityType);
@@ -138,13 +112,5 @@ public class MainController{
 			e.printStackTrace();
 		}
 
-	}
-
-	private List checkObjects(DXFLayer layer, String entityType) {
-	    List plines = layer.getDXFEntities(entityType);
-	    if(plines == null) {
-	        return plines;
-	    }
-        return plines;
 	}
 }
